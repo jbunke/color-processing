7bfcc3a6ab81e847e08b1080f9ad39b128e14c3a
Jordan Bunke <jordantb99@gmail.com>
2025-02-26 04:59:58 +0000

    + Added `quantizeToPalette()`

--- src/com/jordanbunke/color_proc/ColorAlgo.java
+++ src/com/jordanbunke/color_proc/ColorAlgo.java
@@ -1,11 +1,14 @@
 package com.jordanbunke.color_proc;
 
 import com.jordanbunke.delta_time.image.GameImage;
+import com.jordanbunke.delta_time.utility.math.MathPlus;
 
 import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+
+import static com.jordanbunke.color_proc.ColorProc.RGB_SCALE;
 
 public final class ColorAlgo {
     public static GameImage run(
@@ -31,5 +34,34 @@ public final class ColorAlgo {
         }
 
         return img.submit();
+    }
+
+    public static Function<Color, Color> quantizeToPalette(
+            final Color[] palette
+    ) {
+        return c -> {
+            if (palette.length == 0 || c.getAlpha() == 0)
+                return c;
+
+            final Color worst = new Color(
+                    (c.getRed() + (RGB_SCALE / 2)) % RGB_SCALE,
+                    (c.getGreen() + (RGB_SCALE / 2)) % RGB_SCALE,
+                    (c.getBlue() + (RGB_SCALE / 2)) % RGB_SCALE,
+                    (c.getAlpha() + (RGB_SCALE / 2)) % RGB_SCALE);
+
+            return MathPlus.findBest(c, worst, cl -> cl,
+                    (c1, c2) -> diff(c1, c) < diff(c2, c), palette);
+        };
+    }
+
+    public static double diff(final Color a, final Color b) {
+        final int MAX_DIFF = RGB_SCALE * 4;
+
+        final int rDiff = Math.abs(a.getRed() - b.getRed()),
+                gDiff = Math.abs(a.getGreen() - b.getGreen()),
+                bDiff = Math.abs(a.getBlue() - b.getBlue()),
+                alphaDiff = Math.abs(a.getAlpha() - b.getAlpha());
+
+        return (rDiff + gDiff + bDiff + alphaDiff) / (double) MAX_DIFF;
     }
 }
